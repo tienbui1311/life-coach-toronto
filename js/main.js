c7c927a0968aa8f1f44a7bf2a5b13fa79a7835cd
Tien Bui <tienbui1311@gmail.com>
2022-04-05 17:44:13 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// DROPDOWN MENUS
+function menuOneShow() {
+  document.getElementById("coaching").classList.toggle("show");
+  document.getElementById("services").classList.remove("show");
+  document.getElementById("contact").classList.remove("show");
+}
+function menuTwoShow() {
+  document.getElementById("services").classList.toggle("show");
+  document.getElementById("coaching").classList.remove("show");
+  document.getElementById("contact").classList.remove("show");
+}
+function menuThreeShow() {
+  document.getElementById("contact").classList.toggle("show");
+  document.getElementById("coaching").classList.remove("show");
+  document.getElementById("services").classList.remove("show");
+}
+function menuHide() {
+  document.getElementById("coaching").classList.remove("show");
+  document.getElementById("services").classList.remove("show");
+  document.getElementById("contact").classList.remove("show");
+}
+
+document
+  .getElementById("coaching-btn")
+  .addEventListener("mouseenter", menuOneShow);
+
+document
+  .getElementById("services-btn")
+  .addEventListener("mouseover", menuTwoShow);
+
+document
+  .getElementById("contact-btn")
+  .addEventListener("mouseover", menuThreeShow);
+
+document.getElementById("coaching").addEventListener("mouseleave", menuHide);
+document.getElementById("services").addEventListener("mouseleave", menuHide);
+document.getElementById("contact").addEventListener("mouseleave", menuHide);
+
+// Side menu
+function sideMenuAppears() {
+  document.getElementById("side-menu").classList.add("show-menu");
+}
+function sideMenuDisappears() {
+  document.getElementById("side-menu").classList.remove("show-menu");
+}
+document
+  .getElementById("menu-trigger")
+  .addEventListener("click", sideMenuAppears);
+document
+  .getElementById("menu-close")
+  .addEventListener("click", sideMenuDisappears);
+// HH Modal
